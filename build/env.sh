a4ef68ffb6c82ce85325f18f6a78ed2efa14165f
masterdubs <contact@aprisme.be>
2017-09-06 12:06:41 +0200

    Update env.sh

--- build/env.sh
+++ build/env.sh
@@ -10,11 +10,11 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/ethereum"
-if [ ! -L "$ethdir/go-ethereum" ]; then
+ethdir="$workspace/src/github.com/pirl"
+if [ ! -L "$ethdir/pirl" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. go-ethereum
+    ln -s ../../../../../. pirl
     cd "$root"
 fi
 
@@ -23,8 +23,8 @@ GOPATH="$workspace"
 export GOPATH
 
 # Run the command inside the workspace.
-cd "$ethdir/go-ethereum"
-PWD="$ethdir/go-ethereum"
+cd "$ethdir/pirl"
+PWD="$ethdir/pirl"
 
 # Launch the arguments with the configured environment.
 exec "$@"
