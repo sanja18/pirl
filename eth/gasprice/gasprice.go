4b67748e54f95507ad682fa3e90cc2ae7d5c3eed
masterdubs <contact@aprisme.be>
2017-09-06 11:53:34 +0200

    Update gasprice.go

--- eth/gasprice/gasprice.go
+++ eth/gasprice/gasprice.go
@@ -22,10 +22,10 @@ import (
 	"sort"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/internal/ethapi"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/pirl/pirl/common"
+	"github.com/pirl/pirl/internal/ethapi"
+	"github.com/pirl/pirl/params"
+	"github.com/pirl/pirl/rpc"
 )
 
 var maxPrice = big.NewInt(500 * params.Shannon)
