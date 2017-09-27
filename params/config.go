6c1b2562f40591d06e836e07e2a979c7de1d2b0c
masterdubs <contact@masterdubs.io>
2017-09-27 21:54:14 +0200

    block hash

--- params/config.go
+++ params/config.go
@@ -33,9 +33,9 @@ var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
 		ChainId:         big.NewInt(3125659152),
-		HomesteadBlock:  big.NewInt(1150000),
-		DAOForkBlock:    big.NewInt(1920000),
-		DAOForkSupport:  true,
+		HomesteadBlock:  big.NewInt(0),
+		DAOForkBlock:    big.NewInt(0),
+		DAOForkSupport:  false,
 		EIP150Block:     big.NewInt(2463000),
 		EIP150Hash:      common.HexToHash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0"),
 		EIP155Block:     big.NewInt(2675000),
